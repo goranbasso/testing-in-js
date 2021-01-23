f20b009f589e484fcc6348932aa9ca7c65ca59ff
goranbasso <goranbasso@gmail.com>
2021-01-23 19:21:13 +0100

    lagt til eksempel som omhandler .toHaveBeenCalled()

---
+++ oppgave_xx_function/App.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+
+export const login = (username, password) => {
+  console.log(`logging in with username ${username} and password ${password}`)
+}
+
+const App = (props) => {
+
+  const [username, setUsername] = useState([])
+  const [password, setPassword] = useState([])
+
+  useEffect(() => {
+    setUsername('')
+    setPassword('')
+  }, [])
+
+  const onUsernameChanged = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const onPasswordChanged = (event) => {
+    const obfuscatedPassword = event.target.value.replace(/./g, '*')
+    setPassword(obfuscatedPassword)
+  }
+
+  const onLogin = () => {
+    console.log('login')
+    // eslint-disable-next-line react/prop-types
+    // props.login(username, password)
+    login(username, password)
+  }
+
+  return (
+    <div>
+      <label>
+        Brukernavn:
+        <input value={username} onChange={onUsernameChanged} data-testid={"username-input"} />
+      </label>
+      <br />
+      <label>
+        Passord:
+        <input value={password} onChange={onPasswordChanged} data-testid={"password-input"} />
+      </label>
+      <br />
+      <button type={"button"} onClick={onLogin}>Login</button>
+    </div>
+  )
+}
+
+export default App
