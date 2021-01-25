9833d4cc938f82a9f82f2cadbfce7a5cc8e7b983
Gøran Basso <goran.basso@kantega.no>
2021-01-25 16:44:50 +0100

    endret litt på oppgave0, som et eksempel på hvordan vi kan strukturere oppgavene

--- oppgave0/index.js
+++ oppgave0/index.js
@@ -1,3 +1,11 @@
+export function identity(a) {
+  return a
+}
+
 export function sum(a, b) {
   return a - b
+}
+
+export function product(a, b) {
+  return a * b
 }
--- oppgave0/index.test.js
+++ oppgave0/index.test.js
@@ -1,5 +1,21 @@
-import { sum } from "."
+import { sum, identity } from "."
 
-it('Returns the sum of the two input variables', () => {
+// quick and dirty example of how the tasks could be structured:
+// - an example test that works fine
+// - another test that fails, need some changes in the code
+// - another test that is not implemented, the code is already implemented
+
+// a-okay example
+it('identity() returns the same value as the input', () => {
+  expect(identity(1)).toEqual(1)
+})
+
+// the code that is being tested is wrong
+it('sum() returns the sum of the two input variables', () => {
   expect(sum(1, 1)).toEqual(2)
+})
+
+// the test is not implemented (but code should, maybe, work fine)
+it('product() returns the product of the two input variables', () => {
+  // implement a test for product()
 })
